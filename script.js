d2168f454171f0a5f3ac00a169d4c55fe37f1b97
Erzhan <aldamatov@gmail.com>
2022-03-16 10:19:52 +1100

    Fortune Teller

---
+++ script.js
@@ -0,0 +1,13 @@
+/* The Fortune Teller
+Why pay a fortune teller when you can just program your fortune yourself?
+
+Store the following into variables: number of children, partner's name, geographic location, job title.
+Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids." */
+
+
+const numberOfChildren = 3;
+const partnerName = "Jika";
+const geoLocation = "Sydney";
+const jobTitle = "Frontend Developer";
+
+console.log("You will be a "+jobTitle+" in " +geoLocation+", and married to " +partnerName +" with " + numberOfChildren + " kids. ")
